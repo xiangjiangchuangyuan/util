202cebf786922419e566a499848c73fac431b912
zml <441430565@qq.com>
2018-04-13 09:27:00 +0800

    add STR

---
+++ src/main/java/com/xjcy/util/STR.java
@@ -0,0 +1,45 @@
+package com.xjcy.util;
+
+public class STR {
+	public static final String EMPTY = "";
+	public static final String DOT = ".";
+	public static final String SLASH_LEFT = "/";
+	public static final String SLASH_RIGHT = "\\";
+	public static final String HYPHEN = "-";
+
+	public static final String STRUTS_IS_LOAD = "struts_context_is_load";
+	public static final String STRUTS_CONTEXT = "struts_context";
+	
+	public static final String HEADER_PRAGMA = "Pragma";
+	public static final String HEADER_ACCEPT_ENCODING = "Accept-Encoding";
+	public static final String HEADER_CACHE_CONTROL = "Cache-Control";
+	public static final String HEADER_CONTENT_TYPE = "Content-Type";
+	public static final String HEADER_DATE = "Date";
+	
+	public static final String ENCODING_GZIP = "gzip";
+	public static final String ENCODING_UTF8 = "UTF-8";
+	
+	public static final String CONTENT_TYPE_TEXT = "text/plain;charset=utf-8";
+	public static final String CONTENT_TYPE_JSON = "application/json;charset=UTF-8";
+	
+	public static final String VAL_NULL = "null";
+	public static final String VAL_UNDEFINED = "undefined";
+	
+	public static final String CLASS_INTEGER = "class java.lang.Integer";
+	public static final String CLASS_DATE = "class java.util.Date";
+	public static final String CLASS_DOUBLE = "class java.lang.Double";
+	public static final String CLASS_LONG = "class java.lang.Long";
+	public static final String CLASS_BOOLEAN = "class java.lang.Boolean";
+	
+	public static final String WEB_CLASS_PATH = "WEB-INF/classes/";
+	
+	public static final String DATE_LONG = "yyyy-MM-dd HH:mm:ss";
+	public static final String DATE_SHORT = "yyyyMMdd";
+	public static final String DATE_RFC822 = "EEE, dd MMM yyyy HH:mm:ss z";
+	public static final String DATE_ISO8601 = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+	
+	public static final String SUBFIX_CLASS = ".class";
+	public static final String SUBFIX_JSP = ".jsp";
+	public static final String SUBFIX_JSPX = ".jspx";
+
+}
